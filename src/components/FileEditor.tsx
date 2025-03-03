94348aa075f7a6bb8c5e6e35a8ea38f4ff248cdf
Markus-Scl <76035547+Markus-Scl@users.noreply.github.com>
2025-03-03 13:11:31 +0100

    wp file editor

--- src/components/FileEditor.tsx
+++ src/components/FileEditor.tsx
@@ -8,7 +8,10 @@ const FileEditor = ({uploadedFile}: FileEditorProps) => {
 
 	return (
 		<div>
-			<h1>My File Editor</h1>
+			<div tabIndex={0} className="collapse collapse-plus bg-base-100 border-base-300 border">
+				<div className="collapse-title font-semibold">How do I create an account?</div>
+				<div className="collapse-content text-sm">Click the "Sign Up" button in the top right corner and follow the registration process.</div>
+			</div>
 		</div>
 	);
 };
